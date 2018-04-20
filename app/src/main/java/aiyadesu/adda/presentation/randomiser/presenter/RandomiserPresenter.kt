9fcc93b986af096d6ccef8e9a55cfcbc9483a341
LAI, Richard <Richard.LAI@suncorp.com.au>
2018-04-20 16:07:36 +1000

    Added set password button
    * Added set password button currently does not do anything

--- app/src/main/java/aiyadesu/adda/presentation/randomiser/presenter/RandomiserPresenter.kt
+++ app/src/main/java/aiyadesu/adda/presentation/randomiser/presenter/RandomiserPresenter.kt
@@ -2,14 +2,19 @@ package aiyadesu.adda.presentation.randomiser.presenter
 
 import aiyadesu.adda.application.Presenter
 import android.util.Log
+import java.security.KeyStore
 
 /**
  * Created by Richard Lai on 19/4/18.
  */
 class RandomiserPresenter : Presenter {
 
+    interface Display {}
+    interface Router {}
+
     override fun onStart() {
-        Log.i("RandomiserPresenter", "onStart")
+        // TODO: Implement KeyStore so that we can store and retrieve the users Random.org API key later
+        val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
     }
 
     override fun onStop() {
